d4bd4f1c8ee4e8165648f3f97e41fabac173388d
uma-uc <uma.apple123@gmail.com>
2025-09-21 12:57:09 -0700

    initial

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// @ts-expect-error process is a nodejs global
+const host = process.env.TAURI_DEV_HOST;
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+  },
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
+});
