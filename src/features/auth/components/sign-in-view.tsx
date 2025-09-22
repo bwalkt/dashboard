685750b3b78f03b56b2bef8923f9ed23a31de755
uma-uc <uma.apple123@gmail.com>
2025-09-21 23:01:30 -0700

    feat: fixes 2 logo and remove debug in index.html

--- src/features/auth/components/sign-in-view.tsx
+++ src/features/auth/components/sign-in-view.tsx
@@ -4,7 +4,7 @@ import { SignIn as ClerkSignInForm } from '@clerk/clerk-react';
 import { GitHubLogoIcon } from '@radix-ui/react-icons';
 import { IconStar } from '@tabler/icons-react';
 import { Link } from 'react-router-dom';
-
+import './login.css'
 export default function SignInViewPage({ stars }: { stars: number }) {
   return (
     <div className='relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0'>
@@ -20,19 +20,7 @@ export default function SignInViewPage({ stars }: { stars: number }) {
       <div className='bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r'>
         <div className='absolute inset-0 bg-zinc-300' />
         <div className='relative z-20 flex items-center text-lg font-medium'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 24 24'
-            fill='none'
-            stroke='currentColor'
-            strokeWidth='2'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            className='mr-2 h-6 w-6'
-          >
-            <path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
-          </svg>
-          Logo
+          <img src="/boardwalktech-logo.svg" className="mr-2 h-16 w-160"/>
         </div>
       </div>
       <div className='flex h-full items-center justify-center p-4 lg:p-8'>
--- src/features/auth/components/sign-up-view.tsx
+++ src/features/auth/components/sign-up-view.tsx
@@ -4,6 +4,7 @@ import { SignUp as ClerkSignUpForm } from '@clerk/clerk-react';
 import { GitHubLogoIcon } from '@radix-ui/react-icons';
 import { IconStar } from '@tabler/icons-react';
 import { Link } from 'react-router-dom';
+import './login.css';
 
 export default function SignUpViewPage({ stars }: { stars: number }) {
   console.log('SignUpViewPage rendering, stars:', stars);
@@ -22,19 +23,7 @@ export default function SignUpViewPage({ stars }: { stars: number }) {
       <div className='bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r'>
         <div className='absolute inset-0 bg-zinc-300' />
         <div className='relative z-20 flex items-center text-lg font-medium'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 24 24'
-            fill='none'
-            stroke='currentColor'
-            strokeWidth='2'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            className='mr-2 h-6 w-6'
-          >
-            <path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
-          </svg>
-          Logo
+          <img src="/boardwalktech-logo.svg" className="mr-2 h-16 w-160"/>
         </div>
       </div>
       <div className='flex h-full items-center justify-center p-4 lg:p-8'>
