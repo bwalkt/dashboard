e78b533a8cac309fb61d911fef9a47f9d9dd942a
uma-uc <uma.apple123@gmail.com>
2025-09-24 10:46:21 -0700

    feat: monorepo

--- src/pages/dashboard/Accounts.tsx
+++
@@ -1,8 +0,0 @@
-export default function AccountsPage() {
-  return (
-    <div className='container p-4'>
-      <h1 className='text-2xl font-bold'>Accounts</h1>
-      <p>Accounts page content goes here</p>
-    </div>
-  );
-}
--- src/pages/dashboard/Settings.tsx
+++
@@ -1,8 +0,0 @@
-export default function SettingsPage() {
-  return (
-    <div className='container p-4'>
-      <h1 className='text-2xl font-bold'>Settings</h1>
-      <p>Settings page content goes here</p>
-    </div>
-  );
-}
