ff43d292bc04087994ae1e38126386cf990c00a2
uma-uc <94931629+uma-uc@users.noreply.github.com>
2025-11-10 17:00:31 -0800

    Update packages/portal/src/lib/table/filterfns.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- packages/portal/src/lib/table/filterfns.ts
+++ packages/portal/src/lib/table/filterfns.ts
@@ -20,7 +20,7 @@ inDateRange.autoRemove = (val: any) =>
 
 export const arrSome: FilterFn<any> = (row, columnId, filterValue) => {
   if (!Array.isArray(filterValue)) return false;
-  return filterValue.some((val) => row.getValue<unknown[]>(columnId) === val);
+  return filterValue.some((val) => row.getValue(columnId) === val);
 };
 
 arrSome.autoRemove = (val: any) => !Array.isArray(val) || !val?.length;
