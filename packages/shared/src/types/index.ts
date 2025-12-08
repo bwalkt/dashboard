b35cb1df536369284e6dfef62d58da388352c50a
maan1996 <jsprtmnn@gmail.com>
2025-12-08 19:59:30 +0530

    fix: update env variables

--- packages/shared/src/types/index.ts
+++ packages/shared/src/types/index.ts
@@ -3,9 +3,7 @@ export * from './order.js'
 
 // Export all product types
 export * from './product.js'
-
-// Export all user types
-export * from './user.js'
-
 // Export all signoz types
 export * from './signoz.js'
+// Export all user types
+export * from './user.js'
